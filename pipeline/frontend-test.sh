d2605fbeb73b16499775a26eaf0c8d2d659b23ae
Kevin Smith <excited_heaps0s@icloud.com>
2023-10-11 18:05:11 -0400

    Update frontend test

--- pipeline/frontend-test.sh
+++ pipeline/frontend-test.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
-cd ../frontend
-npm install
-npm test --watchAll
+
+if [ -d "../frontend" ]; then
+    cd ../frontend
+    npm install
+    npm test --watchAll
+fi
+
+if [ -d "noaa-weather-pipeline" ]; then
+    cd noaa-weather-pipeline/frontend
+    npm install
+    npm test --watchAll
+fi
