f9a96c3b565ac4291e110f19aa6f85ae44a865e2
Kevin Smith <excited_heaps0s@icloud.com>
2023-10-11 18:16:51 -0400

    Unit test frontend

--- pipeline/frontend-test.sh
+++ pipeline/frontend-test.sh
@@ -4,11 +4,11 @@
 if [ -d "../frontend" ]; then
     cd ../frontend
     npm install
-    npm test --watchAll
+    CI=true npm test --watchAll
 fi
 
 if [ -d "noaa-weather" ]; then
     cd noaa-weather/frontend
     npm install
-    npm test --watchAll
+    CI=true npm test --watchAll
 fi 
